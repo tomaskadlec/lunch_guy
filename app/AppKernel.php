e16d73f31fc920664103fba5431af8f6a0e64910
Tomáš Kadlec <kadleto2@fit.cvut.cz>
2016-02-02 10:13:56 +0100

    Added knplabs/knp-menu-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Net\TomasKadlec\d2sBundle\NetTomasKadlecd2sBundle(),
         ];
 
